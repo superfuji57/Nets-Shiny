5d42fd0e9d3f115557cff8d3fc3075692246915f
Andrew Won <andy.won@gmail.com>
2014-10-26 18:48:10 -0400

    added reactive text

--- server.R
+++ server.R
@@ -4,11 +4,18 @@ library(rCharts)
 load("./data/nets.Rda")
 
 shinyServer(function(input, output) {
-        
+        dataInput <- reactive({
+                subset(nets, Season == input$Season)
+        })
         output$chart <- renderChart2({
-                m1 <- mPlot(x = "DateC", y = c("Nets", "Opp"), type = "Line", data=subset(nets, Season == input$Season))
+                m1 <- mPlot(x = "DateC", y = c("Nets", "Opp"), type = "Line", data=dataInput())
                 m1$set(pointSize = 0, lineWidth = 1, lineColors=c("red", "blue"))
                 return(m1)
+        })
+        output$text1 <- renderText({
+                #"This is Text"
+                paste("During the", input$Season, "season, the Nets averaged", 
+                      round(mean(dataInput()$Nets),1), "points per game.")
                 
         })
 })
--- ui.R
+++ ui.R
@@ -9,7 +9,9 @@ shinyUI(fluidPage(
                         selectInput("Season", "Choose a season",
                                     choices=levels(nets$Season),
                                     selected = "2013"
-                                    )
+                                    ),
+                        
+                        textOutput("text1")
                         ),
         mainPanel(
                 showOutput("chart", "Morris")
